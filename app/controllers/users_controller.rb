6ace2e628578fa70f0b07a92d1c7376b8d7ab447
Scott <scotthuston1982@gmail.com>
2018-02-20 12:08:38 -0500

    added signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,16 @@ class UsersController < ApplicationController
   end
 
   def new
-
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    @user.kind = params[:kind]
 
+    if @user.save
+      redirect_to users_url
+    end
   end
 
   def update
@@ -22,5 +27,9 @@ class UsersController < ApplicationController
 
   def destroy
 
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation,)
   end
 end

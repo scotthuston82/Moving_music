d2a9b8f887d92e4f5c2100d794cbd202ea191af7
Danny Chang <dannychachang@gmail.com>
2018-02-20 14:55:29 -0500

    Add user edit page and link to edit, update controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,8 +21,17 @@ class UsersController < ApplicationController
     end
   end
 
-  def update
+  def edit
+    @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update!(user_params)
+
+    if @user.save
+      redirect_to user_url
+    end
   end
 
   def destroy
@@ -30,6 +39,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation,)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :bio, :profile_picture)
   end
 end

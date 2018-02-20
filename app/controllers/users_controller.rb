1535a9a430e4e94fb76b972b747495555360e96d
Danny Chang <dannychachang@gmail.com>
2018-02-20 15:11:48 -0500

    Add ensure method for editing profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
 
+  before_action :ensure_user_profile_owner, only: [:edit, :update, :destroy]
+
   def index
-    @users = User.all
+    @users = User.where('kind = ?',  'musician')
   end
 
   def show
@@ -22,7 +24,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = current_user
   end
 
   def update
@@ -37,6 +39,15 @@ class UsersController < ApplicationController
   def destroy
 
   end
+
+  def ensure_user_profile_owner
+    @user = User.find(params[:id])
+    if session[:user_id] != @user.id
+      flash[:alert] = ["This is not your profile"]
+      redirect_to users_url
+    end
+  end
+
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :bio, :profile_picture)

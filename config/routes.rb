31bad3e1a9a660bac577290599380dacf46e3a38
Scott <scotthuston1982@gmail.com>
2018-02-21 10:27:08 -0500

    Added a home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+root "users#home"
+
+
+  resources :musicians
   resources :users
   resources :sessions, only: [:new, :create]
   delete 'logout' => 'sessions#destroy'

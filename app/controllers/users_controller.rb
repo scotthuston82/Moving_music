861cdf46fdd1f70a0a760935740fc40deb5169fe
jeffrothwell <rothwelljeff@gmail.com>
2018-02-23 11:36:44 -0500

    add act_type to user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -72,7 +72,7 @@ class UsersController < ApplicationController
 
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :bio, :profile_picture, :stage_name, :hourly_rate, genre_ids: [])
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :bio, :profile_picture, :stage_name, :hourly_rate, genre_ids: [], :act_type)
   end
 
   def empty_profile_picture?

164d43ef469bec290f8dd9f6fda50801f221da2d
jeffrothwell <rothwelljeff@gmail.com>
2018-02-27 14:34:40 -0500

    correct start_date and end_date validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,21 +5,19 @@ class Booking < ApplicationRecord
   has_one :review
 
   validates :address, presence: true
-  validate :cannot_be_in_time_past
+  validate :start_time_cannot_be_in_time_past
   validate :end_time_must_be_after_start
 
 
-  def cannot_be_in_time_past
-    if self[:start_time] > self[:end_time]
-      errors.add(:end_time, "The date cannot be in the past. Please try again.")
+  def start_time_cannot_be_in_time_past
+    if self[:start_time] < Time.now
+      errors.add(:start_time, "The start date cannot be in the past. Please try again.")
     end
   end
 
   def end_time_must_be_after_start
-    if :start_time == :end_time
-      if :start_time.hour >= :end_time.hour
+    if self[:end_time] < self[:start_time]
         errors.add(:end_time, "Please choose an end time after the start time.")
-      end
     end
   end
 

03810f832041b9a2778f889edf7ca899a36ff68b
jeffrothwell <rothwelljeff@gmail.com>
2018-02-27 14:34:04 -0500

    write tests for user validations, update user factory

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -18,6 +18,31 @@ class UserTest < ActiveSupport::TestCase
   test "email must be present" do
     user = build(:user, email: nil)
     user.save
-    refute user.persisted?
+    refute user.persisted?, "user should not persist without email present"
   end
+
+  test "kind must be present" do
+    user = build(:user, kind: nil)
+    user.save
+    refute user.persisted?, "user should not persist if kind is nil"
+  end
+
+  test "kind must only be musician or client" do
+    user = build(:user, kind: 'another kind')
+    user.save
+    refute user.persisted?, "user should not persist if kind is not 'musician' or 'client'"
+  end
+
+  test "musician act_type can only be dj or band" do
+    user = build(:user, act_type: 'another type')
+    user.save
+    refute user.persisted?, "user should not save if act type is not 'dj' or 'band'"
+  end
+
+  test "hourly rate must be greater than zero" do
+    user = build(:user, hourly_rate: 0)
+    user.save
+    refute user.persisted?, "user hourly_rate must be greater than zero"
+  end
+
 end

f0cbcc065eb8fbe52c89333c99cabb7936ebb6fe
jeffrothwell <rothwelljeff@gmail.com>
2018-02-27 23:26:20 -0500

    change hourly rate validation to only run for musicians

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   validates :email, :first_name, :last_name, presence: true
   validates :email, uniqueness: { case_sensitive: false }
   validates :kind, :presence => { message: 'specify if you are a musician or a client!' }
-  validates :hourly_rate, numericality: { greater_than: 0 }
   validate :kind_must_only_be_musician_or_client
   validate :musician_act_type_can_only_be_dj_or_band
+  validate :musician_hourly_rate_must_be_greater_than_zero
 
   has_many :gigs, class_name: 'Booking', foreign_key: :musician_id
   has_many :events, class_name: 'Booking', foreign_key: :client_id
@@ -27,6 +27,14 @@ class User < ApplicationRecord
     end
   end
 
+  # NEED TEST FOR THIS!
+  def musician_hourly_rate_must_be_greater_than_zero
+    if self[:kind] == 'musician' && self[:hourly_rate] && self[:hourly_rate] <= 0
+      errors.add(:hourly_rate, 'must be greater than zero')
+    end
+  end
+
+  # NEED TEST FOR THIS!
   def self.filtre_musicians(act_type, hourly_rate, genres_array)
     genre_musicians = []
     genres_array.delete("") #collection checkboxes always gives empty string as first value. Don't ask why

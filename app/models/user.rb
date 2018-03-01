2d85f1d2a5cb7515090bb970cc53c51848f1700f
jeffrothwell <rothwelljeff@gmail.com>
2018-03-01 15:33:33 -0500

    remove flitre_musicians and re-configure musician validations to only run on update

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,12 @@ class User < ApplicationRecord
   validates :email, :first_name, :last_name, presence: true
   validates :email, uniqueness: { case_sensitive: false }
   validates :kind, :presence => { message: 'specify if you are a musician or a client!' }
+  validates :act_type, presence: true, on: :update, if: :user_type_is_musician?
+  validates :hourly_rate, presence: true, on: :update, if: :user_type_is_musician?
+  validates :hourly_rate, numericality: { greater_than: 0 }, on: :update, if: :user_type_is_musician?
   validate :kind_must_only_be_musician_or_client
-  validate :musician_act_type_can_only_be_dj_or_band
-  validate :musician_hourly_rate_must_be_greater_than_zero
+  validate :musician_act_type_can_only_be_dj_or_band, on: :update, if: :user_type_is_musician?
+  validate :musician_address_must_be_valid, on: :update, if: :user_type_is_musician?
 
   has_many :gigs, class_name: 'Booking', foreign_key: :musician_id
   has_many :events, class_name: 'Booking', foreign_key: :client_id
@@ -25,42 +28,23 @@ class User < ApplicationRecord
   end
 
   def musician_act_type_can_only_be_dj_or_band
-    if self[:kind] == 'musician' && self[:act_type] && self[:act_type] != 'dj' && self[:act_type] != 'band'
+    if self[:kind] == 'musician' && self[:act_type] != 'dj' && self[:act_type] != 'band'
       errors.add(:act_type, "can only be 'dj' or 'band'")
     end
   end
 
-  # NEED TEST FOR THIS!
-  def musician_hourly_rate_must_be_greater_than_zero
-    if self[:kind] == 'musician' && self[:hourly_rate] && self[:hourly_rate] <= 0
-      errors.add(:hourly_rate, 'must be greater than zero')
+  def musician_address_must_be_valid
+    results = Geocoder.search(self[:address])
+    unless results.any?
+      errors.add(:address, 'must be valid')
     end
   end
 
-  def self.filtre_musicians(act_type, hourly_rate, genres_array, array_of_musicians)
-  genre_musicians = []
-  genres_array.delete("") #collection checkboxes always gives empty string as first value. Don't ask why
-  musicians = User.where("kind = ? AND act_type = ? AND hourly_rate <= ?", "musician", act_type, hourly_rate)
-  musicians = musicians.as_json
-  if genres_array.size > 0
-    genres_array.each do |genre_id|
-      genre_musicians << musicians.joins(:genres).where(genres: {id: genre_id})
-    end
-    musicians = genre_musicians.reduce do |common_musicians, genre_list|
-      common_musicians & genre_list
+  def user_type_is_musician?
+    if self[:kind] == 'musician'
+      return true
+    else
+      return false
     end
   end
-  final_musicians_list = []
-  musicians.each do |musician_a|
-    array_of_musicians.each do |musician_b|
-      if musician_a[:id] == musician_b[:id]
-        user = User.new
-        print user.from_json(musician_a.to_json)
-        final_musicians_list << user.from_json(musician_a.to_json)
-      end
-    end
-  end
-  return final_musicians_list.uniq
-end
-
 end

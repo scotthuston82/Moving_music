bba36bc558de2fd561206fbbcc38ab2e1fe09959
Danny Chang <dannychachang@gmail.com>
2018-03-01 18:51:04 -0500

    moved config into the class itself

--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,10 @@ module MovingMusic
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.serve_static_assets = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
 end
-
-config.serve_static_assets = true

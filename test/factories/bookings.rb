57f3e76d5e3722a1c323f9e269f7f9f6005cbff1
jeffrothwell <rothwelljeff@gmail.com>
2018-03-01 15:37:44 -0500

    create bookings factory

--- test/factories/bookings.rb
+++ test/factories/bookings.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :booking do
     address         "220 King St West Toronto, ON"
+    start_time      Time.now + 2.days
+    end_time        Time.now + 2.days + 3.hours
+    party_size      100
+    age_range       "19+"
+    # user
+    # client_user
   end
 end

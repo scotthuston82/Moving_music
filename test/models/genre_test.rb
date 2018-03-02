e029f0349352c391587ad642350a6417b22b0a86
Danny Chang <dannychachang@gmail.com>
2018-03-02 13:28:31 -0500

    need that css

--- test/models/genre_test.rb
+++ test/models/genre_test.rb
@@ -4,9 +4,4 @@ class GenreTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  30.times do
-    test "mock assertion" do
-      assert(true, true)
-    end
-  end
 end

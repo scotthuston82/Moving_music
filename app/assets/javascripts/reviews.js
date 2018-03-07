b9bed4174d2af02a28dc7c3ab69fb17809fb683c
Danny Chang <dannychachang@gmail.com>
2018-03-07 15:44:57 -0500

    some stying for the form, js to pop up the Modal, fixing text area size to 5 rows

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,2 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+document.addEventListener('DOMContentLoaded', function(){
+
+  if (document.querySelector('.write_review')) {
+
+    function showReviewModal(e) {
+      if (e.target.innerText === "Write review") {
+        e.preventDefault();
+        const reviewForm = e.target.parentNode.querySelector('.review_form');
+        reviewForm.classList.toggle('hidden');
+      }
+    }
+
+    past_bookings.addEventListener('click', showReviewModal)
+
+
+  }
+
+})

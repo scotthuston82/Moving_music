de6827ae8de5f86da65b73b7b82f633870a75b54
jeffrothwell <rothwelljeff@gmail.com>
2018-03-07 13:04:06 -0500

    add js to show and hide musician-specific form elements in new.html.erb

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,22 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+document.addEventListener('DOMContentLoaded', function(){
+  if (document.querySelector('#new_user')) {
+    var radioButtons = document.querySelector('#user-kind-radio-buttons')
+    var musicianRadioButton = document.querySelector('#kind_musician')
+    var clientRadioButton = document.querySelector('#kind_client')
+    var musicianFieldsDiv = document.querySelector('.musician-only-fields')
+    console.log(radioButtons);
+    radioButtons.addEventListener('click', function(e){
+      if (musicianRadioButton.checked === true) {
+        if (musicianFieldsDiv.classList.contains('hidden')) {
+          musicianFieldsDiv.classList.remove('hidden')
+          musicianFieldsDiv.classList.add('shown')
+        }
+      } else if (musicianRadioButton.checked === false) {
+        if (musicianFieldsDiv.classList.contains('shown')) {
+          musicianFieldsDiv.classList.remove('shown')
+          musicianFieldsDiv.classList.add('hidden')          
+        }
+      }
+    })
+  }
+})

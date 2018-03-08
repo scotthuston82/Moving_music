d61d6fc97074cd86de6bf3ae6097b8c6627aa2fb
jeffrothwell <rothwelljeff@gmail.com>
2018-03-08 10:33:55 -0500

    add presence validation for event_name

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,7 +4,7 @@ class Booking < ApplicationRecord
   belongs_to :client, class_name: 'User'
   has_one :review
 
-  validates :address, presence: true
+  validates :address, :event_name, presence: true
   validate :start_time_cannot_be_in_time_past
   validate :end_time_must_be_after_start
 

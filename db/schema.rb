e39bb415c26ce7ee0bcdab5734f5970df8fe1de4
jeffrothwell <rothwelljeff@gmail.com>
2018-03-09 12:00:26 -0500

    run equipment migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180308144329) do
+ActiveRecord::Schema.define(version: 20180309165036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 20180308144329) do
     t.datetime "updated_at", null: false
     t.boolean "confirmed", default: false
     t.string "event_name"
+  end
+
+  create_table "equipment", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "category"
+    t.string "details"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "genres", force: :cascade do |t|

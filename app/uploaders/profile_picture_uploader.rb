9626058101b59880197aabb58ddeb75578a67726
Danny Chang <dannychachang@gmail.com>
2018-03-10 09:21:55 -0500

    added a default profile picture using carrierwave

--- app/uploaders/profile_picture_uploader.rb
+++ app/uploaders/profile_picture_uploader.rb
@@ -14,15 +14,12 @@ class ProfilePictureUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+     ActionController::Base.helpers.asset_path("empty_profile.png")
+  end
 
   # Process files as they are uploaded:
-  process scale: [200, 300]
+  # process scale: [200, 300]
   #
   # def scale(width, height)
   #   # do something

49421b49dcbaa20d52bc9c0ef335924837b2d7a0
jeffrothwell <rothwelljeff@gmail.com>
2018-04-26 21:02:25 -0400

    save all checkboxes to a variable

--- app/assets/javascripts/booking_confirmation.js
+++ app/assets/javascripts/booking_confirmation.js
@@ -1,7 +1,8 @@
 $(document).on('turbolinks:load', function(){
   if (document.querySelector('#bookings_confirmation')) {
     if (document.querySelector('.equipment_booking')) {
-      console.log("I see some equipment");
+      var equipmentCheckboxes = document.querySelectorAll('.checkmark')
+      console.log(equipmentCheckboxes);
     }
   }
 })

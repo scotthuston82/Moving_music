adca3d88df9b858a55532cd37926385d9d362d7a
jeffrothwell <rothwelljeff@gmail.com>
2018-04-26 21:25:36 -0400

    add event listener to checkboxes, save equipment cost in a variable

--- app/assets/javascripts/booking_confirmation.js
+++ app/assets/javascripts/booking_confirmation.js
@@ -1,8 +1,21 @@
 $(document).on('turbolinks:load', function(){
+  // First make sure we're on confirm bookings and that there is some equipment
   if (document.querySelector('#bookings_confirmation')) {
     if (document.querySelector('.equipment_booking')) {
+
+      function updatePrice(e){
+        var equipmentPrice = parseInt(
+          e.target.parentNode.querySelector('.booking-price').innerText.split("$").pop()
+        )
+        if (this.checked) {
+          console.log("I'm checked");
+        } else {
+          console.log("Unchecked");
+        }
+      }
+
       var equipmentCheckboxes = document.querySelectorAll('.checkmark')
-      console.log(equipmentCheckboxes);
+      equipmentCheckboxes.forEach(checkbox => checkbox.addEventListener('change', updatePrice))
     }
   }
 })

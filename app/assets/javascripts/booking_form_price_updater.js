fa5b0a7d73a5188c972133fb8c7365bb9f2643e2
jeffrothwell <rothwelljeff@gmail.com>
2018-05-01 10:22:37 -0400

    store all time selectors in variables and put into an array

--- app/assets/javascripts/booking_form_price_updater.js
+++ app/assets/javascripts/booking_form_price_updater.js
@@ -2,29 +2,78 @@
 // form.  Its job is to keep the price of the gig updated as start_time,
 // end_time or any of the equipment selections have changed for the event
 
+// $(document).on('turbolinks:load', function(){
+//   // First make sure we're on confirm bookings and that there is some equipment
+//   if (document.querySelector('#bookings_confirmation')) {
+//     if (document.querySelector('.equipment_booking')) {
+//
+//       function updatePrice(e){
+//         var equipmentPrice = parseInt(
+//           e.target.parentNode.querySelector('.equipment-cost').innerText.split("$").pop()
+//         )
+//         var currentPrice = parseInt(
+//           document.querySelector('#booking-price').innerText.split("$").pop()
+//         )
+//
+//         if (this.checked) {
+//           document.querySelector('#booking-price').innerText = "Total Price: $" + (currentPrice + equipmentPrice)
+//         } else {
+//           document.querySelector('#booking-price').innerText = "Total Price: $" + (currentPrice - equipmentPrice)
+//         }
+//       }
+//
+//       // get an array of all checkboxes and add event listeners to each
+//       var equipmentCheckboxes = document.querySelectorAll('.checkmark')
+//       equipmentCheckboxes.forEach(checkbox => checkbox.addEventListener('change', updatePrice))
+//     }
+//   }
+// })
+
 $(document).on('turbolinks:load', function(){
-  // First make sure we're on confirm bookings and that there is some equipment
-  if (document.querySelector('#bookings_confirmation')) {
-    if (document.querySelector('.equipment_booking')) {
+  // First make sure we're on confirm booking or new booking page
+  if (document.querySelector('#bookings_confirmation') || document.querySelector('#bookings_new')) {
+    var artistHourlyRate = parseInt(document.querySelector('#artist-hourly-rate').innerText)
 
-      function updatePrice(e){
-        var equipmentPrice = parseInt(
-          e.target.parentNode.querySelector('.equipment-cost').innerText.split("$").pop()
-        )
-        var currentPrice = parseInt(
-          document.querySelector('#booking-price').innerText.split("$").pop()
-        )
+    // get all date select fields into variables
+    var startYearSelect = document.querySelector('#booking_start_time_1i')
+    var startMonthSelect = document.querySelector('#booking_start_time_2i')
+    var startDaySelect = document.querySelector('#booking_start_time_3i')
+    var startHourSelect = document.querySelector('#booking_start_time_4i')
+    var startMinuteSelect = document.querySelector('#booking_start_time_5i')
+    var endYearSelect = document.querySelector('#booking_end_time_1i')
+    var endMonthSelect = document.querySelector('#booking_end_time_2i')
+    var endDaySelect = document.querySelector('#booking_end_time_3i')
+    var endHourSelect = document.querySelector('#booking_end_time_4i')
+    var endMinuteSelect = document.querySelector('#booking_end_time_5i')
 
-        if (this.checked) {
-          document.querySelector('#booking-price').innerText = "Total Price: $" + (currentPrice + equipmentPrice)
-        } else {
-          document.querySelector('#booking-price').innerText = "Total Price: $" + (currentPrice - equipmentPrice)
-        }
+    var timeSelectorsArray = [startYearSelect]
+    timeSelectorsArray.push(startMonthSelect)
+    timeSelectorsArray.push(startDaySelect)
+    timeSelectorsArray.push(startHourSelect)
+    timeSelectorsArray.push(startMinuteSelect)
+    timeSelectorsArray.push(endYearSelect)
+    timeSelectorsArray.push(endMonthSelect)
+    timeSelectorsArray.push(endDaySelect)
+    timeSelectorsArray.push(endHourSelect)
+    timeSelectorsArray.push(endMinuteSelect)
+
+    function updatePrice(e){
+      var equipmentPrice = parseInt(
+        e.target.parentNode.querySelector('.equipment-cost').innerText.split("$").pop()
+      )
+      var currentPrice = parseInt(
+        document.querySelector('#booking-price').innerText.split("$").pop()
+      )
+
+      if (this.checked) {
+        document.querySelector('#booking-price').innerText = "Total Price: $" + (currentPrice + equipmentPrice)
+      } else {
+        document.querySelector('#booking-price').innerText = "Total Price: $" + (currentPrice - equipmentPrice)
       }
-
-      // get an array of all checkboxes and add event listeners to each
-      var equipmentCheckboxes = document.querySelectorAll('.checkmark')
-      equipmentCheckboxes.forEach(checkbox => checkbox.addEventListener('change', updatePrice))
     }
+
+    // get an array of all checkboxes and add event listeners to each
+    var equipmentCheckboxes = document.querySelectorAll('.checkmark')
+    equipmentCheckboxes.forEach(checkbox => checkbox.addEventListener('change', updatePrice))
   }
 })

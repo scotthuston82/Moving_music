406ae3c2afe998a86635e828755bc58b31fc905e
jeffrothwell <rothwelljeff@gmail.com>
2018-05-01 10:33:33 -0400

    add event listeners to date selects and make sure changes trigger updatePrice

--- app/assets/javascripts/booking_form_price_updater.js
+++ app/assets/javascripts/booking_form_price_updater.js
@@ -33,6 +33,8 @@ $(document).on('turbolinks:load', function(){
   // First make sure we're on confirm booking or new booking page
   if (document.querySelector('#bookings_confirmation') || document.querySelector('#bookings_new')) {
     var artistHourlyRate = parseInt(document.querySelector('#artist-hourly-rate').innerText)
+    var totalPriceSentenceEle = document.querySelector('#booking-price')
+    var equipmentCheckboxes = document.querySelectorAll('.checkmark')
 
     // get all date select fields into variables
     var startYearSelect = document.querySelector('#booking_start_time_1i')
@@ -58,22 +60,11 @@ $(document).on('turbolinks:load', function(){
     timeSelectorsArray.push(endMinuteSelect)
 
     function updatePrice(e){
-      var equipmentPrice = parseInt(
-        e.target.parentNode.querySelector('.equipment-cost').innerText.split("$").pop()
-      )
-      var currentPrice = parseInt(
-        document.querySelector('#booking-price').innerText.split("$").pop()
-      )
-
-      if (this.checked) {
-        document.querySelector('#booking-price').innerText = "Total Price: $" + (currentPrice + equipmentPrice)
-      } else {
-        document.querySelector('#booking-price').innerText = "Total Price: $" + (currentPrice - equipmentPrice)
-      }
+      console.log("Ch ch ch ch CHANGES");
     }
 
     // get an array of all checkboxes and add event listeners to each
-    var equipmentCheckboxes = document.querySelectorAll('.checkmark')
     equipmentCheckboxes.forEach(checkbox => checkbox.addEventListener('change', updatePrice))
+    timeSelectorsArray.forEach(timeSelect => timeSelect.addEventListener('change', updatePrice))
   }
 })

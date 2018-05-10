9d66e74691d19c054618ab88cdea6d1b8aae6b8c
jeffrothwell <rothwelljeff@gmail.com>
2018-05-10 15:49:01 -0400

    remove carrierwave requires

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
-require 'carrierwave/orm/activerecord'
-require 'carrierwave/processing/mini_magick'
+
 # Initialize the Rails application.
 Rails.application.initialize!
